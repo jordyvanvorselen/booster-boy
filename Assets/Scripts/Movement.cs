3a6deaf435b14009dddd515ef4508a850f8c19d0
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-04-30 13:02:32 +0200

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float thrustPower;
+    [SerializeField] float rotationSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustPower * Time.deltaTime, ForceMode.Force);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    void ApplyRotation(float rotation)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
