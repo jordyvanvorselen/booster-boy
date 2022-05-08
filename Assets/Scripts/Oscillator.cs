64bd4faaca43b2a7e7a8938763e1aebe4d278d6c
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-05-08 16:37:42 +0200

    Lecture 58

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    Vector3 startingPos;
     [SerializeField] Vector3 movementVector;
-    [SerializeField][Range(0, 1)] float movementFactor;
+    [SerializeField] float period = 2f;
+
+    Vector3 startingPos;
+    float movementFactor;
 
     void Start()
     {
@@ -15,6 +17,12 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
+        float cycles = Time.time / period; // continually growing over time
+        const float tau = Mathf.PI * 2; // constant value of 6.283...
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2; // recalculated to go from 0 (starting point) to 1
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
